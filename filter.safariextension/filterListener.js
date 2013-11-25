0f64ca89c70382d2757622791b12301bebde31da
Alberto Morales <alberto@moralitos.com>
2013-11-25 18:11:30 -0500

    initial commit

---
+++ filter.safariextension/filterListener.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  var parseResults = function() {
+    // read the search links
+    console.log($('.r a').size());
+
+  };
+
+  parseResults();
+
+});
---
+++ filter.safariextension/global.js
@@ -0,0 +1,5 @@
+
+
+function getFilters() {
+  alert($);
+};
